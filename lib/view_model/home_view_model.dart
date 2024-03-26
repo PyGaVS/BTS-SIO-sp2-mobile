a9fa32e825c76ed8af284df993243d2dbb313347
unknown <BALL27Ly@sio.lan>
2024-03-26 18:30:36 +0100

    update HomeViewModel

--- lib/view_model/home_view_model.dart
+++ lib/view_model/home_view_model.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:selenium_chat/model/chat.dart';
 
 class HomeViewModel extends ChangeNotifier {
 
-  //late Future<List<AAA>> categories;
+  late Future<List<Chat>> categories;
+
+  HomeViewModel(){
+  }
+
 }
